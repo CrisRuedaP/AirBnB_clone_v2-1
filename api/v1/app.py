e265e6136a3e4a7262663417fbef1a7a4081a48d
David <1508@holbertonschool.com>
2020-09-09 14:21:27 -0500

    Add app

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,21 +2,23 @@
 """
 app file for the api
 """
-
-from flask import Flask
-from flask import Blueprint
-from models import storage
-from api.v1.views import app_views
 from os import getenv
-
+from models import storage
+from flask import Blueprint
+from flask import Flask, jsonify
+from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
 def teardown_appcontext(self):
+    """ method to handle @app.teardown_appcontext """
     storage.close()
 
-
-if __name__ == "__main__":
-    app.run(host=getenv('0.0.0.0'), port='5000', threaded=True, debug=True)
+if __name__ == '__main__':
+    """should not be executed when imported"""
+    HBNB_API_HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    HBNB_API_PORT = getenv('HBNB_API_PORT', '5000')
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT,
+            threaded=True, debug=True)

7cdf96d460378ac7e3c61edd79a80b0d4cab9e35
David <1508@holbertonschool.com>
2020-09-09 10:09:48 -0500

    Fix env variables

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 app file for the api
 """
 
+from os import getenv
 from flask import Flask
 from flask import Blueprint
 from models import storage
@@ -19,6 +20,6 @@ def teardown_appcontext(self):
 
 if __name__ == "__main__":
     app.run(
-            host=getenv('HBNB_API_HOST'),
-            port=getenv('HBNB_API_PORT'),
+            host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(getenv('HBNB_API_PORT', 5000)),
             threaded=True)
